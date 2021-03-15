5febe0696c8ffc24ff4f1661187ec69f3d8e0287
neelu <neelamh1000@gmail.com>
2021-03-15 18:40:48 +0530

    Add files via upload

--- src/components/react-assignment/AssignmentComponents/CurrDateTime.js
+++ src/components/react-assignment/AssignmentComponents/CurrDateTime.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import { ListGroup} from 'react-bootstrap';
 
-const CurrDateTime = () => {
-    return (
-        <div>
-           currentdate and time 
-        </div>
-    )
-}
+const CurrDateTime=()=>
+{
+const currDate = new Date().toDateString()
+const currTime = new Date().toLocaleTimeString()
+return(
+    <div className="container">
+    <div className="row dttime">
+    
+  
+    <ListGroup as="ul"  variant="flush" className="mr-auto col-sm-12 col-md-12 col-lg-4 col-xl-4 mt-2">
+  <ListGroup.Item as="li" active>
+  <h5 className=" mr-auto"> Date : {currDate} </h5>
+  </ListGroup.Item>
+</ListGroup>
 
+<ListGroup as="ul"  variant="flush" className="ml-auto col-sm-12 col-md-12 col-lg-4 col-xl-4 mt-2 ">
+  <ListGroup.Item as="li" active>
+<h5 className="ml-auto ">India Time : {currTime}</h5>
+</ListGroup.Item>
+</ListGroup>
+</div>
+
+</div>
+);
+
+}
 export default CurrDateTime;
